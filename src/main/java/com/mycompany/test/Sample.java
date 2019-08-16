8116e41d6b07c465c1cd4d048822cc830ee99a82
Raju Kumar <M1017259@mindtree.com>
2019-08-16 23:24:39 +0100

    adding files

---
+++ src/main/java/com/mycompany/test/Sample.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.test;
+
+/**
+ *
+ * @author M1017259
+ */
+public class Sample {
+    
+    public int addition (int firstNum, int secondNum){
+        return firstNum + secondNum;
+    }
+}
